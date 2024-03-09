12d1d3459f329261693f2351fbf6a68e40d14dc4
joshtopher <jefriedl@buffalo.edu>
2024-03-09 16:23:32 +0100

    Base Functionality

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # PiM
-This is a re-visit of one of my previous projects. Initially, the project was a Discord bot that helped manage discord servers, however this bot will attempt to use sentiment analysis to tag malicious messages and report them to the server owner.
+This is a re-visit of one of my previous projects 'pybot'. Initially, the project was a Discord bot that helped manage discord servers, however this bot will attempt to use sentiment analysis to tag malicious messages and report them to the server owner.
+'pybot' URL: https://github.com/joshtopher/pybot
+
+
+To add PiM to your discord server, use this URL. PiM needs administrator privileges for full functionality.
+URL: https://discord.com/oauth2/authorize?client_id=1215686228331073587&permissions=8&scope=bot
+
+
+# Goals
+1) To assess the difference in python programming, programming practices, and programming readability with my current self and myself and myself before taking any higher level computer science classes('pybot').
+2) To create a practical tool that I am able to use for myself and share with others.
+3) To become more familiar with sentiment analysis tools used in a course I am currently taking: 'Text Mining for AI' at Vrije Universiteit Amsterdam.
+
+# Current Features
+- General server management functionality
+
+# Planned Features
+- Sentiment Analysis
+- Keeping track of malicious messages and reporting them to moderators
+
+
---
+++ main.py
@@ -0,0 +1,76 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+import discord
+from discord import Intents
+
+# Load discord_token from dotenv file
+load_dotenv()
+token: str = os.getenv('DISCORD_TOKEN')
+
+# Initiate client
+intents: Intents = Intents.default()
+intents.message_content = True # NOQA
+bot = commands.Bot(command_prefix="?pim ", intents=intents, ignore_whitespace=True)
+
+
+# Basic catch-all for unknown/unimplemented commands
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.channel.send("Unknown command.")
+    else:
+        await ctx.channel.send("Something went wrong. Please refer to '?help' to check if the command is being used properly.")
+
+
+# Prints to terminal when bot is ready
+@bot.event
+async def on_ready():
+    print(f"{bot.user} is ready and online!")
+
+
+# Allows users with manage_roles permissions to give a new role to a user
+@bot.command(aliases=['giverole'])
+@commands.has_permissions(manage_roles=True)
+async def give_role(ctx: commands.Context, member: discord.Member, role: discord.Role):
+    await member.add_roles(role)
+    await ctx.channel.send(f"{member.name} is now a {role.name}")
+
+
+# Allows users with manage_roles permissions to remove a role from a user
+@bot.command(aliases=['removerole'])
+@commands.has_permissions(manage_roles=True)
+async def remove_role(ctx: commands.Context, member: discord.Member, role: discord.Role):
+    await member.remove_roles(role)
+    await ctx.channel.send(f"{member.name} is no longer a {role.name}")
+
+
+# Allows users with sufficient permissions to kick members
+@bot.command()
+@commands.has_permissions(kick_members=True)
+async def kick(ctx: commands.Context, member: discord.Member, reason: str):
+    await member.kick(reason=reason)
+    await ctx.channel.send(f"Kicked [{member.name}] for {reason}")
+
+
+# Allows users with sufficient permissions to ban members
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx: commands.Context, member: discord.Member, reason: str):
+    await member.ban(reason=reason)
+    await ctx.channel.send(f"Banned [{member.name}] for {reason}")
+
+
+# This command reads through all messages from other users - this is where sentiment analysis will be used
+@bot.event
+async def on_message(message):
+    # Process commands before analyzing message
+    await bot.process_commands(message)
+    # do not self-reply
+    if message.author == bot.user:
+        return
+    await message.channel.send("Message Received")
+
+
+bot.run(token)
+
